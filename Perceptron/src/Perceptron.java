768a0f31049ed3b93e1ef04ab0eedbde15541a17
tshi206 <tshi206@aucklanduni.ac.nz>
2017-11-03 09:09:02 +1300

    done

--- Perceptron/src/Perceptron.java
+++ Perceptron/src/Perceptron.java
@@ -1,11 +1,16 @@
 import java.io.BufferedInputStream;
 import java.io.InterruptedIOException;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
 
 /**
  * Created by tshi206 on 2/11/2017.
+ *
+ * for binary target features only.
  */
 public class Perceptron {
 
@@ -15,34 +20,93 @@ public class Perceptron {
         System.out.print("number of epoch: "+"\n");
         int epoch = 0;
         try{
-            epoch = Integer.valueOf(scanner.nextLine());
+            epoch = Integer.valueOf(scanner.nextLine().trim());
         }catch (NumberFormatException e){
-            System.out.print("FUCK U");
+            e.printStackTrace();
         }
-        ArrayList<ArrayList<Integer>> dataSet = new ArrayList<ArrayList<Integer>>();
+        System.out.print("number of input features plus target feature: "+"\n");
+        int inpFeature = 0;
+        try{
+            inpFeature = Integer.valueOf(scanner.nextLine().trim());
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+        System.out.print("initial weight vector: "+"\n");
+        ArrayList<Integer> initialVec = new ArrayList<>();
+        try{
+            String temp = scanner.nextLine();
+            String[] parsedTemp = temp.split("\\s+");
+            int tempCount = 0;
+            for (String s : parsedTemp) {
+                if (s.equals("") || s.matches("\\s+")){
+                    continue;
+                }
+                initialVec.add(Integer.valueOf(s.trim()));
+                tempCount++;
+            }
+            if (tempCount != inpFeature){
+                throw new RuntimeException("number of input features and feature count doesn't match.");
+            }
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+        System.out.print("start taking input rows. input an empty line to quit."+"\n");
+        ArrayList<ArrayList<Integer>> dataSet = new ArrayList<>();
         try{
             while (scanner.hasNextLine()){
                 String input = scanner.nextLine();
-                String[] parsedInput = input.split("\\s+");
-                ArrayList<Integer> temp = new ArrayList<>();;
-                for (String word : parsedInput){
-                    temp.add(Integer.valueOf(word));
+                if (input.equals("") || input.matches("\\s+")){
+                    break;
                 }
-                dataSet.add(temp);
+                int featuresCount = 0;
+                String[] parsedInput = input.split("\\s+");
+                ArrayList<Integer> temp = new ArrayList<>();
+                for (String word : parsedInput){
+                    if (word.equals("") || word.matches("\\s+")){
+                        continue;
+                    }
+                    temp.add(Integer.valueOf(word.trim()));
+                    featuresCount++;
+                }
+                if (featuresCount != inpFeature){
+                    throw new RuntimeException("number of input features and feature count doesn't match.");
+                }else{
+                    dataSet.add(temp);
+                }
             }
         }catch (NumberFormatException e){
-            System.out.print("OK\n");
+            e.printStackTrace();
         }finally {
             scanner.close();
         }
+        System.out.print("#####################################################\n");
+        System.out.print("Initial Weight Vector: \n");
+        System.out.print("[ ");
+        Queue<Integer> tempQueue = new ConcurrentLinkedQueue<>();
+        initialVec.forEach(integer -> tempQueue.add(integer));
+        String preparedStatement = "";
+        while (!tempQueue.isEmpty()){
+            preparedStatement = preparedStatement + tempQueue.poll() + ", ";
+        }
+        preparedStatement = preparedStatement.substring(0, preparedStatement.length()-2);
+        System.out.print(preparedStatement);
+        System.out.print(" ]\n");
+        for (int i=0; i< inpFeature; i++){
+            if (i == inpFeature-1){
+                System.out.print("Target Feature"+"\t\n");
+                break;
+            }
+            System.out.print("Input Feature "+i+"\t");
+        }
         dataSet.forEach(aRow -> {
             aRow.forEach(integer -> {
-                System.out.print(integer + " ");
+                System.out.print(integer + "\t\t\t\t");
             });
             System.out.print("\n");
         });
-        Solver solver = new Solver(epoch, dataSet);
-
+        System.out.print("#####################################################\n\n\n");
+        Solver solver = new Solver(epoch, dataSet, inpFeature, initialVec);
+        solver.solveIt();
     }
 
 }
--- Perceptron/src/Solver.java
+++ Perceptron/src/Solver.java
@@ -1,19 +1,72 @@
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by tshi206 on 2/11/2017.
  */
 public class Solver {
 
-        int numOfEpoch;
-        ArrayList<ArrayList<Integer>> dataSet = new ArrayList<ArrayList<Integer>>();
+    int numOfEpoch;
+    ArrayList<ArrayList<Integer>> dataSet = new ArrayList<ArrayList<Integer>>();
+    int featuresCount;
+    ArrayList<Integer> initialVector = new ArrayList<>();
 
-        public Solver(int numOfEpoch, ArrayList<ArrayList<Integer>> dataSet){
-            this.numOfEpoch = numOfEpoch;
-            this.dataSet = dataSet;
+    public Solver(int numOfEpoch, ArrayList<ArrayList<Integer>> dataSet, int featuresCount, ArrayList<Integer>
+            initialVector){
+        this.numOfEpoch = numOfEpoch;
+        this.dataSet = dataSet;
+        this.featuresCount = featuresCount;
+        this.initialVector = initialVector;
+    }
+
+    public void solveIt(){
+        for (int i=0; i<numOfEpoch; i++){
+            dataSet.forEach(rows -> {
+                int classification = initialVector.get(0);
+                for (int j=1; j<initialVector.size(); j++){
+                    classification += initialVector.get(j) * rows.get(j - 1);
+                }
+                int pclass;
+                if (classification > 0){
+                    pclass = 1;
+                }else{
+                    pclass = 0;
+                }
+                ArrayList<Integer> temp = new ArrayList<>();
+                switch (rows.get(rows.size()-1) - pclass){
+                    case 0:
+                        break;
+                    case 1:
+                        temp.add(initialVector.get(0) + 1);
+                        for (int k=1; k<initialVector.size(); k++){
+                            temp.add(initialVector.get(k) + rows.get(k - 1));
+                        }
+                        initialVector.clear();
+                        initialVector.addAll(temp);
+                        break;
+                    case -1:
+                        temp.add(initialVector.get(0) - 1);
+                        for (int k=1; k<initialVector.size(); k++){
+                            temp.add(initialVector.get(k) - rows.get(k - 1));
+                        }
+                        initialVector.clear();
+                        initialVector.addAll(temp);
+                        break;
+                }
+                System.out.print("Weight Vector: ");
+                System.out.print("[ ");
+                Queue<Integer> tempQueue = new ConcurrentLinkedQueue<>();
+                initialVector.forEach(integer -> tempQueue.add(integer));
+                String preparedStatement = "";
+                while (!tempQueue.isEmpty()){
+                    preparedStatement = preparedStatement + tempQueue.poll() + ", ";
+                }
+                preparedStatement = preparedStatement.substring(0, preparedStatement.length()-2);
+                System.out.print(preparedStatement);
+                System.out.print(" ]\n");
+            });
+            System.out.print("\nepoch " + (i + 1) + " finished.\n\n");
         }
-
-        public void solveIt(){
-
-        }
+    }
 }
